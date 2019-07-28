1641179fff400afbe6480607bfb637e397ac1f63
nikithaameda <48934090+nikithaameda@users.noreply.github.com>
2019-07-28 00:18:48 -0400

    Portfolio initial commit

---
+++ script.js
@@ -0,0 +1,15 @@
+$(document).ready(function(){
+    var navDisplay = $( "#technical-skills" );
+    var offset = navDisplay.offset();
+    $(".navbar").hide();
+    $(window).scroll(function () {
+        if ($(this).scrollTop() >= offset.top - 140) {
+            $('.navbar').show();
+        } else {
+            $('.navbar').fadeOut();
+        }
+    });
+    $(".hamburger").click(function() {
+        $(".hamburger-navbar").toggleClass("show")
+    });
+});
